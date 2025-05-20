b763af9f9313fb00f1d2c4624e89c042166c0568
arthur10o <arthur.pruvost@protonmail.com>
2025-05-20 19:24:19 +0200

    update programme

--- program/program.ino
+++ program/program.ino
@@ -19,10 +19,12 @@ int trigPin = 9;
 int echoPin = 10;
 
 int nb_measure = 10;
-int minimum_distance = 40;          // distance in cm at which robot must stop
+int minimum_distance = 60;          // distance in cm at which robot must stop
 
 long left_distance, right_distance;
-long slight_left_distance, slight_right_distance;
+
+unsigned long lastMillis = 0;
+const long interval = 150;         // Waiting time in ms between distance measurements.
 
 long quickselect(long arr[], int left, int right, int k);
 int partition(long arr[], int left, int right);
@@ -36,38 +38,38 @@ Car_state car_state;
 struct Direction_servo_motor {
     const char* name;
     int angle;
+    int last_position;
 };
 
 Direction_servo_motor FRONT = {"front", 120};
-Direction_servo_motor TO_RIGHT = {"right", 30};
-Direction_servo_motor SLIGHT_RIGHT = {"slight_right", 75};
-Direction_servo_motor TO_LEFT = {"left", 210};
-Direction_servo_motor SLIGHT_LEFT = {"slight_left", 165};
-Direction_servo_motor SLIGHT_MIDLE_LEFT = {"slight_midle_left", 142};
-Direction_servo_motor SLIGHT_MIDLE_RIGHT = {"slight_midle_right", 98};
+Direction_servo_motor TO_RIGHT = {"right", 115};
+Direction_servo_motor TO_LEFT = {"left", 125};
 Direction_servo_motor servo_state = FRONT;
 
 Direction_servo_motor direction_to_check[] = {
-    SLIGHT_MIDLE_LEFT,
-    SLIGHT_RIGHT,
+    TO_RIGHT,
     FRONT,
-    SLIGHT_LEFT,
-    SLIGHT_MIDLE_RIGHT
+    TO_LEFT,
+    FRONT,
 };
 
 const int nb_direction = sizeof(direction_to_check) / sizeof(direction_to_check[0]);
 int distance[nb_direction];
 
+const int MAX_CACHED_ANGLES = 10;
+int cached_angles[MAX_CACHED_ANGLES];
+long cached_distances[MAX_CACHED_ANGLES];
+int cached_count = 0;
+
 long measure_distance(int nb_measure, const char* direction) {
     /**
-     * Measures the distance between the robot and an obstacle using an ultrasonic sensor.
-     * The function performs multiple measurements and returns the average value.
-     * 
-     * @param nb_measure Number of individual distance measurements to average.
-     * @param direction  Direction label.
-     * @return median in centimeters (as a long integer).
-     */
-
+    * Measures the distance between the robot and an obstacle using an ultrasonic sensor.
+    * The function performs multiple measurements and returns the average value.
+    * 
+    * @param nb_measure Number of individual distance measurements to average.
+    * @param direction  Direction label.
+    * @return median in centimeters (as a long integer).
+    */
     long measures[20];
     int count = min(nb_measure, 20);
 
@@ -102,16 +104,25 @@ long measure_distance(int nb_measure, const char* direction) {
         return 0;
     }
 
-    Serial.print("Distance ");
+    /*Serial.print("Distance ");
     Serial.print(direction);
     Serial.print(": ");                             // DEBUG
     Serial.print(median);
-    Serial.println(" cm");
+    Serial.println(" cm");*/
 
     return median;
 }
 
 int partition(long arr[], int left, int right) {
+    /**
+    * Partitions an array around a pivot for use in the quickselect algorithm.
+    * Elements smaller than the pivot are moved to the left, and larger to the right.
+    *
+    * @param arr   The array to partition.
+    * @param left  Starting index of the subarray.
+    * @param right Ending index of the subarray (pivot element).
+    * @return The index position of the pivot after partitioning.
+    */
     long pivot = arr[right];
     int i = left;
     for (int j = left; j < right; j++) {
@@ -129,6 +140,16 @@ int partition(long arr[], int left, int right) {
 }
 
 long quickselect(long arr[], int left, int right, int k) {
+    /**
+    * Selects the k-th smallest element from the array using the Quickselect algorithm.
+    * This is used for efficiently finding the median in unsorted data.
+    *
+    * @param arr   The array to search.
+    * @param left  Starting index of the subarray.
+    * @param right Ending index of the subarray.
+    * @param k     The index (0-based) of the desired smallest element.
+    * @return The k-th smallest element in the array.
+    */
     if (left == right) return arr[left];
     int pivotIndex = partition(arr, left, right);
     if (k == pivotIndex) return arr[k];
@@ -136,6 +157,22 @@ long quickselect(long arr[], int left, int right, int k) {
     else return quickselect(arr, pivotIndex + 1, right, k);
 }
 
+long get_cached_distance(int angle) {
+    for (int i = 0; i < cached_count; i++) {
+        if (cached_angles[i] == angle) {
+            return cached_distances[i];
+        }
+    }
+    return -1;
+}
+
+void add_cached_distance(int angle, long distance) {
+    if (cached_count < MAX_CACHED_ANGLES) {
+        cached_angles[cached_count] = angle;
+        cached_distances[cached_count] = distance;
+        cached_count++;
+    }
+}
 
 void stopp() {
     /**
@@ -196,6 +233,10 @@ void turn_left(int time) {
 }
 
 void setup() {
+    /**
+    * Initializes the robot's hardware configuration.
+    * Sets up pins, attaches the servo motor, and defines initial movement state.
+    */
     Serial.begin(9600);
 
     pinMode(ENB, OUTPUT);
@@ -212,67 +253,74 @@ void setup() {
     myservo.write(servo_state.angle);
 
     car_state.last_moove = STOP;
+    delay(1000);
 }
 
 void loop() {
+    /**
+    * Main control loop of the robot.
+    * If moving forward, constantly checks distances to obstacles.
+    * If stopped, scans surroundings and chooses a direction with the most space to resume movement.
+    */
     if(car_state.last_moove == ADVANCE) {
-        advance();
-
+        bool obstacle_detected = false;
+        cached_count = 0;
         for (int i = 0; i < nb_direction; i++) {
             servo_state = direction_to_check[i];
             myservo.write(servo_state.angle);
+            servo_state.last_position = servo_state.angle;
             delay(150);
 
-            distance[i] = measure_distance(nb_measure / 5, servo_state.name);
+            long cached = get_cached_distance(servo_state.angle);
+            if (cached == -1) {
+                cached = measure_distance(nb_measure / 5, servo_state.name);
+                add_cached_distance(servo_state.angle, cached);
+            }
 
-            if (distance[i] <= minimum_distance) {
-                car_state.last_moove = STOP;
-                stopp();
+            distance[i] = cached;
+
+            if (distance[i] <= minimum_distance || distance[i] >= 300) {
+                obstacle_detected = true;
                 break;
             }
+        }
+        if (obstacle_detected) {
+            car_state.last_moove = STOP;
+            stopp();
+        } else {
+            advance();
         }
     } else if(car_state.last_moove == STOP) {
         stopp();
 
         car_state.last_moove = STOP;
 
-         Direction_servo_motor direction_verify_obstacle[] = {
-            SLIGHT_MIDLE_LEFT,
-            TO_LEFT,
-            TO_RIGHT,
-            SLIGHT_LEFT,
-            SLIGHT_RIGHT
-        };
-
-        long* distances_target[] = {
-            &left_distance,
-            &left_distance,
-            &right_distance,
-            &slight_left_distance,
-            &slight_right_distance
-        };
-
-        for (int i = 0; i < 5; i++) {
-            servo_state = direction_verify_obstacle[i];
+        for (int i = 0; i < sizeof(direction_to_check) / sizeof(direction_to_check[0]); i++) {
+            servo_state = direction_to_check[i];
             myservo.write(servo_state.angle);
+            servo_state.last_position = servo_state.angle;
             delay(150);
 
-            *distances_target[i] = measure_distance(nb_measure, servo_state.name);
+            long cached = get_cached_distance(servo_state.angle);
+            if (cached == -1) {
+                cached = measure_distance(nb_measure, servo_state.name);
+                add_cached_distance(servo_state.angle, cached);
+            }
+
+            distance[i] = cached;
         }
 
-        long max_dist = max(max(left_distance, right_distance), max(slight_left_distance, slight_right_distance));
+        long max_dist = max(left_distance, right_distance);
 
         if (max_dist > minimum_distance) {
             if (max_dist == left_distance) turn_left(255);
-            else if (max_dist == slight_left_distance) turn_left(125);
             else if (max_dist == right_distance) turn_right(255);
-            else if (max_dist == slight_right_distance) turn_right(125);
             car_state.last_moove = ADVANCE;
         } else {
             Serial.println("Area blocked !");
             stopp();
             car_state.last_moove = STOP;
+            cached_count = 0;
         }
-
     }
 }
